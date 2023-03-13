14b79fc9ec49beebc3c9b32c8f60b96281a017cb
Italo <italoxbobs@hotmail.com>
2023-03-13 14:50:58 +0100

    comment deleted

--- binary2.py
+++ binary2.py
@@ -5,6 +5,6 @@ binario = "" # inicializa a variável binario como uma string vazia
 while decimal > 0: # enquanto o decimal for maior que zero
     resto = decimal % 2 # calcula o resto da divisão por 2
     binario = str(resto) + binario # adiciona o resto na frente do número binário
-    decimal //= 2 # divide o decimal por 2 (divisão inteira)
+    decimal //= 2 # divide o decimal por 2 (divisão inteira
 
 print(f"O número {decimal} em binário é {binario}")
